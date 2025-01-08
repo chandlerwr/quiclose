58b48f462d10f95df96ea3dea778859ad13093d1
Chandler Riehm <15848544+chandlerwr@users.noreply.github.com>
2025-01-08 17:04:06 -0500

    Initial WPF app

---
+++ Quiclose/Closer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiclose {
+    internal class Closer {
+
+        private Process? _process = null;
+
+        public Closer(string processName) {
+            ProcessName = processName;
+            Poll();
+        }
+
+        public string ProcessName { get; }
+        public bool HasProcess => _process != null;
+
+        public void Poll () {
+
+            var ps = Process.GetProcessesByName(ProcessName);
+
+            if (ps.Length == 0) {
+                Debug.WriteLine($"\n\n\nNo processed named {ProcessName} found");
+                _process = null;
+                return;
+            }
+
+            if (ps.Length > 1) {
+                Debug.WriteLine($"\n\n\nMany processes named {ProcessName} found");
+                _process = null;
+                return;
+            }
+
+            _process = ps[0];
+
+        }
+
+        public void Close() {
+            if (!HasProcess) throw new InvalidOperationException();
+
+            _process?.Kill();
+
+        }
+
+    }
+}
---
+++ Quiclose/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiclose {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public MainWindow () {
+            InitializeComponent();
+
+            var ps = Process.GetProcessesByName("Notepad");
+            string o = ps.Length != 0 ? ("Found: " + string.Join(", ", ps.Select(p => p.Id))) : "NONE FOUND!";
+
+            Debug.WriteLine($"\n*****\n\n{o}\n\n*****\n");
+
+            if (ps.Length == 1) ps[0].Kill();
+        }
+
+    }
+}
