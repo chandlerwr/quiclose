2bc883de57d75f1edb933cce9f8ed9e11aa2d7d6
Chandler Riehm <15848544+chandlerwr@users.noreply.github.com>
2025-03-24 20:39:47 -0400

    Added localization support and new keybinds

---
+++ Quiclose/VKLocalizer.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiclose {
+
+    /// <summary>
+    /// Normally done with a converter. Can't be bothered.
+    /// </summary>
+    internal partial class VKLocalizer : ObservableObject {
+
+        private readonly ResourceManager _rm = new(typeof(Localization.Main));
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(DisplayString))]
+        private VK _key;
+
+        public string DisplayString => _rm.GetString($"VK_Label_{Key}") ?? "UNKNOWN?";
+
+    }
+}
